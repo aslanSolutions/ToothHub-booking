65903cb3bf956d523e657951c5d17fb0657b7eab
Aslan <aslan@luday.se>
2023-11-13 01:08:22 +0100

    #18 Added CRUD routes

--- Backend/Services/Notification/api/routes.py
+++ Backend/Services/Notification/api/routes.py
@@ -7,8 +7,29 @@ bp = Blueprint('notification', __name__, url_prefix='/notifications')
 notification_schema = NotificationSchema()
 
 @bp.route('/<int:notification_id>', methods=['GET'])
-@other_responses({404: 'Notification details not found'})
+@other_responses({404: 'Notification not found'})
 @response(notification_schema, 200)
-def get_delivery(notification_id):
-    """retrieve a Notification by id"""
+def get_notification(notification_id):
+    """Retrieve a notification by id"""
+    return "Hello World"
+
+@bp.route('/', methods=['GET'])
+@other_responses({404: 'Notification not found'})
+@response(notification_schema, 200)
+def get_notifications(notification_id):
+    """Retrieve all notifications list"""
+    return "Hello World"
+
+
+@bp.route('/', methods=['POST'])
+@other_responses({501: 'Notification could not be sent'})
+@response(notification_schema, 201)
+def post_notification(notification_id):
+    """Create a notification"""
+    return "Hello World"
+
+@bp.route('/<int:notification_id>', methods=['DELETE'])
+@other_responses({404: 'Notification not found'})
+def delete_notification(notification_id):
+    """Delete a notification by id"""
     return "Hello World"
