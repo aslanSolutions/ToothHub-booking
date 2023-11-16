723ceb0ca581e5b25688d41bbaedc4b3a0b39e34
Aslan <muhmmad@student.chalmers.se>
2023-11-16 10:51:38 +0100

    #17 adding the broker address and the topic

--- Backend/Services/Notification/api/__init__.py
+++ Backend/Services/Notification/api/__init__.py
@@ -3,12 +3,14 @@ from apifairy import APIFairy
 from flask_marshmallow import Marshmallow
 from .routes import bp as notification_bp
 from .mqtt import mqtt_client, socketio
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
 
 apifairy = APIFairy()
 ma = Marshmallow()
 
-brokerAdress = None
-brokerPort = None
+brokerAdress = "0169ad6feac84c25b5b11b5157be1bd8.s2.eu.hivemq.cloud"
+brokerPort = 8883
 
 def create_app():
     app = Flask(__name__)
@@ -19,7 +21,8 @@ def create_app():
     apifairy.init_app(app)
     ma.init_app(app)
     socketio.init_app(app)
-
+    db = client.flask_db
+    todos = db.todos  
     # Register the notification blueprint
     app.register_blueprint(notification_bp)
 
@@ -27,13 +30,12 @@ def create_app():
     app.config['MQTT_BROKER_ADDRESS'] = brokerAdress
     app.config['MQTT_PORT'] = brokerPort
 
-    # Connecting the MQTT client
-    mqtt_client.connect(app.config['MQTT_BROKER_ADDRESS'], app.config['MQTT_PORT'])
-
     # Add the MQTT client to the app context
     app.mqtt_client = mqtt_client
 
     # Start the MQTT client loop in a separate thread
+    # Connecting the MQTT client
+    mqtt_client.connect(app.config['MQTT_BROKER_ADDRESS'], app.config['MQTT_PORT'])
     mqtt_client.loop_start()
 
     return app
