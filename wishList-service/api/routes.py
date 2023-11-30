132a143471282b4368a375db1d4fa14e5314b0fe
nawrask <nawrask@student.chalmers.se>
2023-11-30 13:07:18 +0100

    routes fixes

--- wishList-service/api/routes.py
+++ wishList-service/api/routes.py
@@ -16,7 +16,7 @@ AUTH_SERVICE_URL = "http://localhost:5005"
 
 @bp.route('/register', methods=['POST'])
 def register_wishlist():
-    # Create Patient account
+    # Create wishlist
     payload = request.get_json()
 
     # Validate the payload against the schema
@@ -27,7 +27,7 @@ def register_wishlist():
     # Make a registration request to the authentication service
     wishlist_service_url = "http://127.0.0.1:5005/wishlist/register"  # Update with the correct URL
     wishlist_payload = {
-        "patient_id": payload["patient_id"],  # Use relevant patient information
+        "patient_id": payload["patient_id"],  # Use relevant wishlist information
         "date": payload["date"]
     }
 
@@ -35,7 +35,7 @@ def register_wishlist():
 
     if wishlist_response.status_code == 201:
         # If registration is successful in the authentication service,
-        # proceed with the patient registration in your service.
+        # proceed with the wishlist registration in your service.
         result = users.insert_one(payload)
         new_patient_id = result.inserted_id
         created_patient = users.find_one({'_id': new_patient_id})
@@ -47,56 +47,37 @@ def register_wishlist():
     else:
         # Handle registration failure in the authentication service
         print(f"Authentication service response: {wishlist_response.text}")
-        return jsonify({"message": "Patient registration failed"}), 500
+        return jsonify({"message": "wishlist registration failed"}), 500
 
-@bp.route('/<int:patient_id>', methods=['GET'])
+@bp.route('/<int:wishlist_id>', methods=['GET'])
 @response(WishlistSchema, 200)
-def get_patient(patient_id):
+def get_wishliat(wishlist_id):
     # Retrieve a dentist by ID
-    patient = users.find_one({'_id': patient_id})
+    wishlist = users.find_one({'_id': wishlist_id})
 
-    if patient:
-        return patient
+    if wishlist:
+        return wishlist
     else:
-        return jsonify({"message": f"No patient found with ID {patient_id}"}), 404
+        return jsonify({"message": f"No wishlist found with ID {wishlist_id}"}), 404
 
-@bp.route('/', methods=['POST'])
-@response(WishlistSchema, 201)
-@body(WishlistSchema, 201)
-def create_patient():
-    # Create Patient account
-    payload = request.get_json()
-
-    # Validate the payload against the schema
-    errors = WishlistSchema.validate(payload)
-    if errors:
-        return jsonify({"message": "Validation error", "errors": errors}), 400
-
-    # Insert the new patient into the MongoDB collection
-    result = users.insert_one(payload)
-    new_patient_id = result.inserted_id
-
-    # Return the created patinet with the generated ID
-    created_patient = users.find_one({'_id': new_patient_id})
-    return created_patient, 201
 
 @bp.route('/', methods=['GET'])
 @response(WishlistSchema, 200)
-def get_all_patient():
+def get_all_wishlists():
     # Retrieve all dentsit list
     all_patinets = list(wishlist_collection.find({}))
     return all_patinets
 
 
-@bp.route('/<int:patient_id>', methods=['PATCH'])
+@bp.route('/<int:wishlist_id>', methods=['PATCH'])
 @response(WishlistSchema, 200)
 @body(WishlistSchema, 200)
-def update_patient(patient_id):
-    # Update a patient by ID
-    existing_patient = users.find_one({'_id': patient_id})
+def update_wishlist(wishlist_id):
+    # Update a wishlist by ID
+    existing_wishlist = users.find_one({'_id': wishlist_id})
 
-    if not existing_patient:
-        return jsonify({"message": f"No patient found with ID {patient_id}"}), 404
+    if not existing_wishlist:
+        return jsonify({"message": f"No wishlist found with ID {wishlist_id}"}), 404
 
     # Get the updated data from the request payload
     updated_data = request.get_json()
@@ -107,25 +88,25 @@ def update_patient(patient_id):
         return jsonify({"message": "Validation error", "errors": errors}), 400
 
     # Update the existing patinet with the new data
-    users.update_one({'_id': patient_id}, {'$set': updated_data})
+    users.update_one({'_id': wishlist_id}, {'$set': updated_data})
 
-    # Return the updated patient
-    updated_patient = users.find_one({'_id': patient_id})
-    return updated_patient
+    # Return the updated wishlist
+    updated_wishlist = users.find_one({'_id': wishlist_id})
+    return updated_wishlist
 
-@bp.route('/<int:patient_id>', methods=['DELETE'])
-def delete_patient(patient_id):
-    # Delete a patient account by ID
-    result = users.delete_one({'_id': patient_id})
+@bp.route('/<int:wishlist_id>', methods=['DELETE'])
+def delete_wishlist(wishlist_id):
+    # Delete a wishlist account by ID
+    result = users.delete_one({'_id': wishlist_id})
 
     if result.deleted_count == 1:
-        return jsonify({"message": f"Patient with ID {patient_id} deleted successfully"}), 200
+        return jsonify({"message": f"wishlist with ID {wishlist_id} deleted successfully"}), 200
     else:
-        return jsonify({"message": f"No dentist found with ID {patient_id}"}), 404
+        return jsonify({"message": f"No dentist found with ID {wishlist_id}"}), 404
 
 @bp.route('/delete_all', methods=['DELETE'])
-def delete_all_patients():
+def delete_all_wishlists():
     # Delete all patinets from the collection
     result = wishlist_collection.delete_many({})
-    return jsonify({"message": f"{result.deleted_count} patients deleted"}), 200
+    return jsonify({"message": f"{result.deleted_count} wishlist deleted"}), 200
 
